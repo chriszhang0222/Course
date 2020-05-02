e49865fdd662c5d129463114a8e62f155f7b8d36
chris <869415122@qq.com>
2020-05-02 20:29:22 +0800

    add gateway

---
+++ gateway/src/main/java/com/chris/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.chris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Chris Zhang
+ * @Date: 5/2/20 19:59
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
