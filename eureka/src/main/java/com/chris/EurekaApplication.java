07b864d0a72d6813caf45d77ed18dd381d32b2d9
chris <869415122@qq.com>
2020-05-02 19:44:14 +0800

    Initial commit

---
+++ eureka/src/main/java/com/chris/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.chris;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Chris Zhang
+ * @Date: 5/2/20 18:29
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+
+    }
+}
