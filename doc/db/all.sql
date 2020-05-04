7fb595979d8a0b80679c7a871bfa7b75db993f89
chris <869415122@qq.com>
2020-05-04 14:45:57 +0800

    add section to database

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -13,3 +13,22 @@ insert into `chapter` (id, course_id, name) VALUES ('000001', '000001', 'Test Ch
 insert into `chapter` (id, course_id, name) VALUES ('000002', '000002', 'Test Chapter4');
 insert into `chapter` (id, course_id, name) VALUES ('000003', '000003', 'Test Chapter5');
 insert into `chapter` (id, course_id, name) VALUES ('000004', '000004', 'Test Chapter6');
+
+
+drop table if exists `section`;
+create table `section`(
+ `id` CHAR(8) not null default '' comment 'ID',
+ `title` VARCHAR(50) not null,
+ `course_id` char(8),
+ `chapter_id` char(8),
+ `video` varchar(200),
+ `time` int,
+ `charge` char(1),
+ `sort` int,
+ `create_at` DATETIME(3),
+ `update_at` DATETIME(3),
+ PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, create_at, update_at)
+    VALUES ('0000001','test section 1', '0000001','0000000','',500,'F',1,now(), now());
---
+++ server/src/main/java/com/chris/mapper/SectionMapper.java
@@ -0,0 +1,33 @@
+package com.chris.mapper;
+
+import com.chris.domain.Section;
+import com.chris.domain.SectionExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface SectionMapper {
+    long countByExample(SectionExample example);
+
+    int deleteByExample(SectionExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Section record);
+
+    int insertSelective(Section record);
+
+    List<Section> selectByExample(SectionExample example);
+
+    Section selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Section record, @Param("example") SectionExample example);
+
+    int updateByExample(@Param("record") Section record, @Param("example") SectionExample example);
+
+    int updateByPrimaryKeySelective(Section record);
+
+    int updateByPrimaryKey(Section record);
+}
