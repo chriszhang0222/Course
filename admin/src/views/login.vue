97f822da2428e356e30aa5dbdce824a0a97e3c14
chriszhang94 <chris@quantumsds.com>
2020-08-06 16:18:54 +0800

    login changed

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -23,54 +23,54 @@
                                             ACE ADMIN
                                         </h4>
 
-                                        <div class="space-6"></div>
+            <div class="space-6"></div>
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
+                <form>
+                    <fieldset>
+                    <label class="block clearfix">
                       <span class="block input-icon input-icon-right">
                         <input type="text" v-model="user.loginName" class="form-control" placeholder="用户名"/>
                         <i class="ace-icon fa fa-user"></i>
                       </span>
-                                                </label>
+                    </label>
 
-                                                <label class="block clearfix">
+                    <label class="block clearfix">
                       <span class="block input-icon input-icon-right">
                         <input v-model="user.password" type="password" class="form-control" placeholder="密码"/>
                         <i class="ace-icon fa fa-lock"></i>
                       </span>
-                                                </label>
+                    </label>
 
-                                                <label class="block clearfix">
-<!--                      <span class="block input-icon input-icon-right">-->
-<!--                        <div class="input-group">-->
-<!--                          <input  type="text" class="form-control" placeholder="验证码">-->
-<!--                          <span class="input-group-addon" id="basic-addon2">-->
-<!--                            <img v-on:click="loadImageCode()" id="image-code" alt="验证码"/>-->
-<!--                          </span>-->
-<!--                        </div>-->
-<!--                      </span>-->
-                                                </label>
+                    <label class="block clearfix">
+                          <span class="block input-icon input-icon-right">
+                            <div class="input-group">
+                              <input type="text" class="form-control" placeholder="kaptcha">
+                              <span class="input-group-addon" id="basic-addon2">
+                                <img v-on:click="loadImageCode()" id="image-code" alt="kaptcha"/>
+                              </span>
+                            </div>
+                          </span>
+                    </label>
 
-                                                <div class="space"></div>
+                <div class="space"></div>
 
-                                                <div class="clearfix">
-                                                    <label class="inline">
-                                                        <input v-model="remember" type="checkbox" class="ace"/>
-                                                        <span class="lbl">Remember Me</span>
-                                                    </label>
+                <div class="clearfix">
+                    <label class="inline">
+                        <input v-model="remember" type="checkbox" class="ace"/>
+                        <span class="lbl">Remember Me</span>
+                    </label>
 
-                                                    <button type="button"
-                                                            class="width-35 pull-right btn btn-sm btn-primary"
-                                                            v-on:click="login()">
-                                                        <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">Login</span>
-                                                    </button>
-                                                </div>
+                    <button type="button"
+                            class="width-35 pull-right btn btn-sm btn-primary"
+                            v-on:click="login()">
+                        <i class="ace-icon fa fa-key"></i>
+                        <span class="bigger-110">Login</span>
+                    </button>
+                </div>
 
-                                                <div class="space-4"></div>
-                                            </fieldset>
-                                        </form>
+                <div class="space-4"></div>
+            </fieldset>
+        </form>
 
                                     </div><!-- /.widget-main -->
 
@@ -93,10 +93,12 @@
               user: {},
               url: process.env.VUE_APP_SERVER,
               error: '',
-              remember: true
+              remember: true,
+              imageToken: ''
           }
         },
         mounted:function(){
+            this.loadImageCode();
             $('body').removeClass('no-skin');
             $('body').attr('class', 'login-layout blur-login');
             let localUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER);
@@ -105,6 +107,10 @@
             }
         },
         methods:{
+            loadImageCode(){
+                this.imageToken = Tool.uuid(8);
+                $('#image-code').attr('src', this.url + '/system/admin/kaptcha/image-code/' + this.imageToken);
+            },
             login(){
                 let vm = this;
                 if(!Validator.require(this.user.loginName, 'loginName')
@@ -113,11 +119,13 @@
                     this.user = {};
                     return;
                 }
+                let md5 = hex_md5(vm.user.password);
+                let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER) || {};
+                if (rememberUser.md5 !== md5){
+                    vm.user.password = hex_md5(vm.user.password + KEY);
+                }
 
                 let url = this.url;
-                let oldPassword = this.user.password;
-                let password = hex_md5(this.user.password + KEY);
-                this.user.password = password;
                 this.$ajax.post(url + '/system/admin/user/login', this.user)
                 .then((res) => {
                     let resp = res.data;
@@ -125,9 +133,11 @@
                         let user = resp.content;
                         SessionStorage.set(SESSION_KEY_LOGIN_USER, user);
                         if(vm.remember){
+                            let md5 = hex_md5(vm.user.password);
                             LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
                                 loginName: vm.user.loginName,
-                                password: oldPassword
+                                password: vm.user.password,
+                                md5: md5
                             });
                         }else{
                             LocalStorage.set(LOCAL_KEY_REMEMBER_USER, null);
@@ -146,5 +156,8 @@
 </script>
 
 <style scoped>
+    .input-group-addon {
+        padding: 0;
+    }
 
 </style>
--- server/src/main/java/com/chris/dto/LoginDto.java
+++ server/src/main/java/com/chris/dto/LoginDto.java
@@ -12,4 +12,5 @@ public class LoginDto {
     private String id;
     private String loginName;
     private String name;
+    private String token;
 }
